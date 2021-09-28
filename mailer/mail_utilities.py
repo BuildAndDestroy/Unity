89b440249fb647c35c9a596db484f9529f38c183
BuildAndDestroy <devreap1@gmail.com>
2021-09-27 18:24:31 -0600

    spearphishing: email out to multiple recipients

--- mailer/mail_utilities.py
+++ mailer/mail_utilities.py
@@ -14,6 +14,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
+import glob
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -22,61 +23,91 @@ from email.mime.text import MIMEText
 class Email(object):
     """Compile an email for sending."""
 
-    def __init__(self, my_email, your_email, subject, text_email_content, html_email_content) -> None:
+    def __init__(self, my_email, my_display_name, your_email, subject, html_email_file) -> None:
         self.my_email = my_email
+        self.my_display_name = my_display_name
         self.your_email = your_email
         self.subject = subject
-        self.text_email_content = text_email_content
-        self.html_email_content = html_email_content
+        self.html_email_file = html_email_file
 
-    @property
-    def compile_email(self) -> str:
-        """Create an eamil in text and html format."""
+    def full_path_file(self) -> list:
+        """If file not found, raise an exception."""
+        full_file_path = glob.glob(f'{self.html_email_file}', recursive=False)
+        if not full_file_path:
+            raise ValueError("File not found!")
+        return full_file_path
+
+    def read_html_file(self) -> str:
+        """Read a file and return the content in memory"""
+        html_email_file = self.full_path_file()
+        with open(html_email_file[0], 'r') as html_file:
+            read_file_html = html_file.read()
+        return read_file_html
+
+    #@property
+    def compile_email(self, your_email) -> str:
+        """Create an email in html format."""
         message = MIMEMultipart('alternative')
         message['Subject'] = self.subject
-        message['From'] = self.my_email
-        message['To'] = self.your_email
-        text = self.text_email_content
-        html = self.html_email_content
-        text_message = MIMEText(text, 'plain')
+        message['From'] = f'{self.my_display_name} {self.my_email}'
+        message['To'] = your_email
+        html = self.read_html_file()
         html_message = MIMEText(html, 'html')
-        message.attach(text_message)
         message.attach(html_message)
         return message
 
 
-class SendEmail(object):
+class SendEmail(Email):
     """Manage connecting and disconnecting to a mail server."""
 
-    def __init__(self, username, password, mailserver, my_email, your_email, ssl, email_content) -> None:
+    def __init__(self, my_email, my_display_name, your_email, subject, html_email_file, username, password, mailserver, ssl) -> None:
+        super().__init__(my_email, my_display_name, your_email, subject, html_email_file)
         self.username = username
         self.password = password
         self.mailserver = mailserver
-        self.my_email = my_email
-        self.your_email = your_email
         self.ssl = ssl
-        self.email_content = email_content
 
     def test_objects(self) -> None:
         """Test user input and verify the content parses."""
-
         print(self.username)
         print(self.password)
         print(self.mailserver)
         print(self.ssl)
         print(self.email_content)
 
+    def username_is_file(self) -> bool:
+        """Check if username is string or file."""
+        full_file_path = glob.glob(f'{self.your_email}', recursive=False)
+        if full_file_path:
+            return True
+        return False
+
+    def list_of_emails(self) -> list:
+        """Open a file full of email addresses and return as a list."""
+        full_file_path = glob.glob(f'{self.your_email}', recursive=False)
+        with open(full_file_path[0], 'r') as file_of_emails:
+            all_user_emails = file_of_emails.readlines()
+            all_user_emails = [line.rstrip() for line in all_user_emails]
+        return all_user_emails
+
     def connect_send_disconnect(self) -> None:
         """Login to a mail server."""
         if self.ssl:
             server = smtplib.SMTP_SSL(self.mailserver, 465)
 
-        else:
+        if not self.ssl:
             server = smtplib.SMTP(self.mailserver, 25)
 
         server.login(self.username, self.password)
-        server.sendmail(self.my_email, self.your_email,
-                        self.email_content.as_string())
-        server.quit()
 
-        print('[*] Email sent!')
+        if self.username_is_file():
+            for index in self.list_of_emails():
+                compiled_email = super().compile_email(index)
+                server.sendmail(self.my_email, index, compiled_email.as_string())
+                print(f'[*] Email sent to {index}!')
+        else:
+            compiled_email = super().compile_email(self.your_email)
+            server.sendmail(self.my_email, self.your_email,
+                            compiled_email.as_string())
+            print(f'[*] Email sent to {self.your_email}!')
+        server.quit()
--- scripts/unity_send_mail.py
+++ scripts/unity_send_mail.py
@@ -27,40 +27,28 @@ def parse_args() -> tuple:
     url = '[*] https://github.com/BuildAndDestroy/Unity'
     epilog = '{}\n\n{}'.format(url, legal_statement)
     parser = argparse.ArgumentParser(
-        description=__doc__, epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
+        epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
 
     subparsers = parser.add_subparsers(help='commands', dest='commands')
 
     single_mailer = subparsers.add_parser(
-        'spear_fish', help='Send a single email.')
+        'spear_fish', help='Send emails.')
     single_mailer.add_argument(
         'my_email', help='The email address you will be sending from as yourself.')
+    single_mailer.add_argument('my_display_name', help='My display name that I want displayed in the email')
     single_mailer.add_argument(
-        'your_email', help='The email address you will be sending to, the recipient.')
+        'your_email', help='The email address, or file full of email addresses, you will be sending to. The recipient.')
     single_mailer.add_argument(
         'mailserver', help='The mailserver we will connect to to send our email.')
     single_mailer.add_argument(
-        'username', help='Set a username to login to the mailserver.Password will be asked on execute,')
+        'username', help='Set a username to login to the mailserver. Password will be asked on execute.')
     single_mailer.add_argument(
         'subject', help='The Subject of your email, put the string in quotes.')
     single_mailer.add_argument(
-        'text_email', help='Type out your email but only text, put the message in quotes.')
-    single_mailer.add_argument(
-        'html_email', help='Craft an email using html, put the string in quotes.')
+        'html_email', help='Full path to your html email.')
 
     single_mailer.add_argument(
         '-s', '--ssl', action='store_true', help='Use encryption to the mailserver.')
-
-    # mass_mailler = subparsers.add_parser('mass_mailler', help='Send an email to a list of emails.')
-    # mass_mailler.add_argument('my_email', help='The email address you will be sending from as yourself.')
-    # mass_mailler.add_argument('your_email', help='A file with email addresses you will be sending to, the recipients.\n\rA new email address on each line of the file.')
-    # mass_mailler.add_argument('mailserver', help='The mailserver we will connect to to send our email.')
-    # mass_mailler.add_argument('username', action='store_true', help='Set a username to login to the mailserver.\n\rPassword will be asked on execute,')
-    # mass_mailler.add_argument('subject', help='The Subject of your email, put the string in quotes.')
-    # mass_mailler.add_argument('text_email', help='Type out your email but only text, put the message in quotes.')
-    # mass_mailler.add_argument('html_email', help='Craft an email using html, put the string in quotes.')
-
-    # mass_mailler.add_argument('-s', '--ssl', action='store_true', help='Use encryption to the mailserver.')
 
     args = parser.parse_args()
     return args
@@ -69,15 +57,19 @@ def parse_args() -> tuple:
 def main() -> None:
     """Request password and ompile emailm then send."""
     args = parse_args()
+    print(args)
 
     password = getpass.getpass()
-
-    email_content = Email(args.my_email, args.your_email,
-                          args.subject, args.text_email, args.html_email)
-    send_email = SendEmail(args.username, password, args.mailserver,
-                           args.my_email, args.your_email, args.ssl, email_content.compile_email)
-    send_email.connect_send_disconnect()
-
+    if args.commands == 'spear_fish':
+       #email_content = Email(args.my_email, args.my_display_name, args.your_email,
+       #                      args.subject, args.html_email)
+       #send_email = SendEmail(args.username, password, args.mailserver,
+       #                       args.my_email, args.my_display_name, args.your_email, args.ssl, email_content.compile_email)
+       #send_email.connect_send_disconnect()
+       send_email = SendEmail(args.my_email, args.my_display_name, args.your_email, args.subject, args.html_email, args.username, password, args.mailserver, args.ssl)
+       send_email.connect_send_disconnect()
+    else:
+        print('[*] Use a positional argument!')
 
 if __name__ == '__main__':
     main()
